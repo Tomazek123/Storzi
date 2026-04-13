421d48ad600df191d7be343a41c325053b8e3d61
Nejc Koščak <own3d.kosci@gmail.com>
2026-04-13 18:12:47 +0200

    Za mazija

---
+++ posts.js
@@ -0,0 +1,42 @@
+var nextAdventures = [
+  {
+    "title": "Smarna",
+    "image": "images/destinations/smarna.webp",
+    "wx": "clear",
+    "estDate": "3. maj 2026",
+    "estTime": "4 ure"
+  }
+];
+
+var postsData = [
+  {
+    "title": "Smarna",
+    "date": "21. marec 2026",
+    "image": "images/destinations/smarna.webp",
+    "wx": "clear"
+  },
+  {
+    "title": "Rakitovec",
+    "date": "11. april 2026",
+    "image": "images/destinations/ratitovec.webp",
+    "wx": "snow"
+  },
+  {
+    "title": "Lovrenc",
+    "date": "14. februar 2026",
+    "image": "images/destinations/lovrenc.webp",
+    "wx": "clear"
+  },
+  {
+    "title": "Grmada",
+    "date": "21. februar 2026",
+    "image": "images/destinations/grmada.webp",
+    "wx": "snow"
+  },
+  {
+    "title": "Krim",
+    "date": "25. januar 2026",
+    "image": "images/destinations/Krim.jpg",
+    "wx": "snow"
+  }
+];
